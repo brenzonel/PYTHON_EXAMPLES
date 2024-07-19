739207ea4a27e09c80b364d228e6ee4173dcbe39
brenzonel <bran.alan@hotmail.com>
2024-07-19 11:09:15 -0600

    Fix: Usando variables de ambiente y no texto

--- Pruebas/Execute_sp_oracle.py
+++ Pruebas/Execute_sp_oracle.py
@@ -1,20 +1,32 @@
 import getpass
 import oracledb
+from dotenv import load_dotenv
+import os
 
 #pw = getpass.getpass("Enter password: ")
+load_dotenv(override=True)
+usrbd = os.getenv('DBORA_USR')
+pwdbd = os.getenv('DBORA_PWD')
+hostbd = os.getenv('DBORA_HOST')
+portbd = os.getenv('DBORA_PORT')
+snamebd = os.getenv('DBORA_SNAME')
 
 connection = oracledb.connect(
-    user="system",
-    password="30Extencion..",
-    host="localhost", port=1521, service_name="orcl")
+    user=usrbd,
+    password=pwdbd,
+    host=hostbd, port=portbd, service_name=snamebd)
 
 print("Successfully connected to Oracle Database")
 
 cursor = connection.cursor()
 
+"""with oracledb.connect(user=un, password=pw, dsn=cs) as connection:
+    with connection.cursor() as cursor:
+        sql = """"select sysdate from dual"""" #son 3 comillas dobles solamente
+        for r in cursor.execute(sql):
+            print(r)"""
+
 # Create a table
-
-
 cursor.execute("""
     create table todoitem (
         id number generated always as identity,
@@ -43,4 +55,4 @@ for row in cursor.execute('select description, done from todoitem'):
     else:
         print(row[0], "is NOT done")
 
-#connection.close()
+connection.close()
